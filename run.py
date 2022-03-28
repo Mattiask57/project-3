ca7f04de14b724be15205bda9da889fd3cf26ea7
Mattiask57 <matte20_@hotmail.com>
2022-03-28 01:57:51 +0000

    Add welcome message, instructions for the game and function to get the game running

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def run_game():
+
+    """
+    Running the game
+    """
+
+print("Welcome to the best tic-tack-toe game out there!\n")
+print("How to play: \n")
+print("You will use a mark (X or O) and place it on the board.")
+print("First one to place three marks in a row wins.")
+print("These underscores is the board positions.")
+print("Top left = 1 and bottom right = 9. Choose a number to place your mark.")
+print("\n_ _ _")
+print("_ _ _")
+print("_ _ _\n")
+run_game()
