70911363b305914786723bcd00fcbed1fd1d8f47
Mattiask57 <matte20_@hotmail.com>
2022-03-28 04:42:53 +0000

    Add check for winner function and adjusted function for draw to respond properly

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ current_player = "X"
 
 winner = None
 # ------- Classes -------
+
+
 class Error(Exception):
     """
     Class for exceptions
@@ -30,74 +32,57 @@ def check_win():
     All possible winning combinations.
     """
     if (board[1] == board[2] and board[1] == board[3] and board[1] != "-"):
-        return True
+        return board[1]
     elif (board[4] == board[5] and board[4] == board[6] and board[4] != "-"):
-        return True
+        return board[4]
     elif (board[7] == board[8] and board[7] == board[9] and board[7] != "-"):
-        return True
+        return board[7]
     elif (board[1] == board[4] and board[1] == board[7] and board[1] != "-"):
-        return True
+        return board[1]
     elif (board[2] == board[5] and board[2] == board[8] and board[2] != "-"):
-        return True
+        return board[2]
     elif (board[3] == board[6] and board[3] == board[9] and board[3] != "-"):
-        return True
+        return board[3]
     elif (board[1] == board[5] and board[1] == board[9] and board[1] != "-"):
-        return True
+        return board[1]
     elif (board[7] == board[5] and board[7] == board[3] and board[7] != "-"):
-        return True
+        return board[7]
     else:
         return False
 
+def check_for_winner():
+    """
+    Checks who has won
+    """
+    global winner
+    checked_winner = check_win()
+    if checked_winner:
+        winner = checked_winner
+    else:
+        winner = None
+
+
 def check_draw():
     """
-    Check if game is draw
+    Check if game is draw, if then draw game over.
     """
+    global game_running
     for key in board.keys():
         if board[key] == "-":
             return False
+    else:
+        game_running = False
+        print("Its a draw!")
 
-    return True
 
 def check_if_game_over():
-    """ Check if win or draw """
-    check_win()
+    """
+    Check if win or draw
+    """
+    check_for_winner()
     check_draw()
 
-def flip_player():
-    """ Swaps the current player """
-    global current_player
-    if current_player == "X":
-        current_player = "O"
-    elif current_player == "O":
-        current_player = "X"
-
-
-def run_game():
-
-    """
-    Running the game
-    """
-    display_board(board)
-
-        while game_running:
-
-        handle_turn(current_player)
-        check_if_game_over()
-        swap_player()
-        if winner == "X" or winner == "O":
-            print(winner + "wins!")
-            exit()
-
-def display_board(board):
-    """
-    Display board
-    """
-    print(board[1] + " | " + board[2] + " | " + board[3])
-    print(board[4] + " | " + board[5] + " | " + board[6])
-    print(board[7] + " | " + board[8] + " | " + board[9])
-    print("\n")
-
-    def handle_turn(player):
+def handle_turn(player):
     """
     Handles player turns and checks valid input.
     """
@@ -110,7 +95,7 @@ def display_board(board):
             if position not in x:
                 raise ValueError
             else:
-                position = int(position) - 1
+                position = int(position) - 0
                 if board[position] == "-":
                     valid = True
                     board[position] = player
@@ -125,12 +110,47 @@ def display_board(board):
             print("\nError: Space Taken, Try Again\n")
             position = input("\nChoose a position from 1-9: ")
 
-
-
-
+def flip_player():
     """
-    Welcome message
+    Swaps the current player
     """
+    global current_player
+    if current_player == "X":
+        current_player = "O"
+    elif current_player == "O":
+        current_player = "X"
+
+
+def display_board(board):
+    """
+    Display board
+    """
+    print(board[1] + " | " + board[2] + " | " + board[3])
+    print(board[4] + " | " + board[5] + " | " + board[6])
+    print(board[7] + " | " + board[8] + " | " + board[9])
+    print("\n")
+
+
+def run_game():
+    """
+    Running the game
+    """
+    display_board(board)
+
+    while game_running:
+
+        handle_turn(current_player)
+        check_if_game_over()
+        flip_player()
+        if winner == "X" or winner == "O":
+            print(winner + "wins!")
+            exit()
+
+
+
+"""
+Welcome message
+"""
 print("\nWelcome to the best tic-tack-toe game out there!\n")
 print("How to play: \n")
 print("You will use a mark (X or O) and place it on the board.")
