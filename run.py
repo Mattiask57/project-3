88510a174786073ef3579a045b888bb6e224e1fd
Mattiask57 <matte20_@hotmail.com>
2022-03-28 02:37:53 +0000

    Add function handle turn to handle turn and check valid inputs

--- run.py
+++ run.py
@@ -25,10 +25,38 @@ def display_board(board):
     print(board[6] + " | " + board[7] + " | " + board[8])
     print("\n")
 
+    def handle_turn(player):
+    """
+    Handles player turns and checks valid input.
+    """
+    print(player + "'s turn.")
+    valid = False
+    position = input("\nChoose a position from 1-9: ")
+    x = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    while not valid:
+        try:
+            if position not in x:
+                raise ValueError
+            else:
+                position = int(position) - 1
+                if board[position] == "-":
+                    valid = True
+                    board[position] = player
+                    display_board()
+                    return
+                else:
+                    raise SpaceTakenError
+        except ValueError:
+            print("\nError: Incorrect Value Please Try Again\n")
+            position = input("\nChoose a position from 1-9: ")
+        except SpaceTakenError:
+            print("\nError: Space Taken, Try Again\n")
+            position = input("\nChoose a position from 1-9: ")
+
     """
     Welcome message
     """
-print("Welcome to the best tic-tack-toe game out there!\n")
+print("\nWelcome to the best tic-tack-toe game out there!\n")
 print("How to play: \n")
 print("You will use a mark (X or O) and place it on the board.")
 print("First one to place three marks in a row wins.")
